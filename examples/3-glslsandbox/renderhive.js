476781bc245fc26d4143d3505935605c89dd96e3
zz85 <zz85nus+dev@gmail.com>
2012-02-21 20:41:17 +0800

    creating tests.

--- examples/3-glslsandbox/renderhive.js
+++ examples/3-glslsandbox/renderhive.js
@@ -1,9 +1,66 @@
 /* 
  * Version 3. Integration with GLSL Sandbox
  */
+var app = require('express').createServer();
+var formidable = require('formidable');
+var fs = require('fs');
+var util = require('util'),
+	spawn = require('child_process').spawn;
 
-function RenderProject() {
+var path = require('path');
 
+// app.configure(function(){
+// 	// Sets the root directory
+//     var dir =  __dirname + '/..';
+//     console.log(dir);
+//     app.set('views', dir);
+//     app.set('view options', {layout: false});
+//     app.set("view engine", "html");
+//  });
+//  
+// app.get('/', function(req, res){
+//   res.sendfile("canvasrender01.html");
+// });
+// 
+// // we send the file, if it match the url
+// app.get(/\/(.*)/, function(req, res){
+//     res.sendfile(req.params[0]);
+// });
+
+
+app.get(/\/(.*)/, function(req, res){
+    res.sendfile(req.params[0]);
+});
+
+app.post('*', function(req, res) {
+	console.log('receive');
+	
+	console.log(req.body);
+});
+
+// Start the server listening on port 8000
+app.listen(8000);
+
+// Project Settings
+function RenderProject(options) {
+	
+	if (options!==undefined) 
+	for (var i in options) {
+		this[i] = options[i];
+	}
+	
+	
+	if (!this.fps) this.fps = 25;
+	if (!this.height) this.height = 720;
+	if (!this.width) this.width = 480;
+	
+	// Project Directory
+	if (!this.stills) this.stills = '_stills';
+	if (!this.renders) this.renders = '_renders';
+	if (!this.assets) this.assets = '_assets';
+	
+	// Random Id / Key?
+	this.frames = this.fps * this.duration;
 
 	
 };
@@ -22,3 +79,5 @@ var project01 = {
     width: 720
 };
 */
+
+var minecraftcubes = new RenderProject();
